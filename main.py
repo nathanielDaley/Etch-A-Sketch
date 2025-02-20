11204218c7d6189ff45b3bf282099d158785f5cc
Nathaniel Daley <ndaley64@gmail.com>
2025-02-20 14:06:39 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import turtle
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    tim.left(10)
+
+def turn_right():
+    tim.right(10)
+
+def clear_screen():
+    turtle.resetscreen()
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear_screen, "c")
+
+screen.exitonclick()
